f59097562d61a39f3a3983d7e2077fec1f0a59f8
Sanko Robinson <sanko@cpan.org>
2021-11-18 01:54:29 -0800

    Basic win32 example

---
+++ eg/win32_screenres.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use lib '../lib', '../blib/arch', '../blib/lib';
+use Dyn qw[:dc :dl :sugar];
+use File::Find;
+$|++;
+#
+my $path = 'C:\Windows\System32\user32.dll';
+my $lib  = dlLoadLibrary($path);
+#
+CORE::say 'width = ' . call( $lib, 'GetSystemMetrics', 'i)i', 0 );
+CORE::say 'height = ' . call( $lib, 'GetSystemMetrics', 'i)i', 1 );
+CORE::say 'number of monitors = ' . call( $lib, 'GetSystemMetrics', 'i)i', 80 );
